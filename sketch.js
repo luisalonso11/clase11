df7ad1197311a165971652f7426ca36252986be4
luisalonso11 <123043231+luisalonso11@users.noreply.github.com>
2023-01-23 19:17:31 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+var trex ,trex_running;
+var ground, invisibleGround, groundImage;
+
+function preload(){
+  trex_running=loadAnimation("trex1.png", "trex3.png", "trex4.png")
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+trex=createSprite(50,160,20,50)
+trex.addAnimation("running",trex_running)
+
+trex.scale=0.5;
+trex.x=50
+
+ground=createSprite(200,180,400,20)
+
+ //crear sprite de Trex
+ 
+}
+
+function draw(){
+  background("white")
+  
+if(keyDown("space")){
+trex.velocityY=-10;
+}
+
+trex.velocityY=trex.velocityY+0.8;
+
+trex.collide(ground);
+
+drawSprites();
+}
